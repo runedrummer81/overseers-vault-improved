9fa7ef3867cf5dbfd1116509eabd2761ca3fc6c5
runedrummer81 <runedrummer81@gmail.com>
2026-03-16 14:17:06 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  // Change base path when building for production
+  if (command !== "serve") {
+    config.base = "/overseers-vault-improved/"; // 👈 Replace with your GitHub repository name
+  }
+
+  return config;
+});
