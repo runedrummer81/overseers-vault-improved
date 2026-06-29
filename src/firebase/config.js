aadc18286321061d05c89053d5c3468afcf5d836
runedrummer81 <runedrummer81@gmail.com>
2026-06-29 21:14:36 +0200

    trying again

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,21 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:
-    import.meta.env.VITE_FIREBASE_API_KEY ??
-    "AIzaSyCIS9Cxw21yjsfVAX47lRibuHBsazBzIlY",
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ??
-    "improved-dnd-vault.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? "improved-dnd-vault",
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ??
-    "improved-dnd-vault.firebasestorage.app",
-  messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ?? "629170727872",
-  appId:
-    import.meta.env.VITE_FIREBASE_APP_ID ??
-    "1:629170727872:web:41106feccd0c383b5df625",
+  apiKey: "AIzaSyCIS9Cxw21yjsfVAX47lRibuHBsazBzIlY",
+  authDomain: "improved-dnd-vault.firebaseapp.com",
+  projectId: "improved-dnd-vault",
+  storageBucket: "improved-dnd-vault.firebasestorage.app",
+  messagingSenderId: "629170727872",
+  appId: "1:629170727872:web:41106feccd0c383b5df625",
 };
 
 const app = initializeApp(firebaseConfig);
